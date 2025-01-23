fecc09886f3149aad0c256a7d67d0fafc6ebc288
Mohsen Banan <form@mohsen.banan.1.byname.net>
2025-01-23 15:09:38 -0800

    0.11 Modernized.

--- py3/setup.py
+++ py3/setup.py
@@ -1,30 +1,56 @@
 #!/usr/bin/env python
 
+# Template File:  /bisos/apps/defaults/begin/templates/purposed/pyModule/python/setup.py
+# Blee Panel: /bisos/git/auth/bxRepos/blee-binders/bisos-core/bisos-pip/bisos.pycs/pipPackaging/_nodeBase_/fullUsagePanel-en.org
+
+# b:py3:pypi:setup/pkgName Arguments  :pkgName "somePkg" :pkgNameSpace "bisos"
+####+BEGIN: b:py3:pypi:setup/pkgName :comment "Auto Detected"
+
 import setuptools
 import re
+import inspect
+import pathlib
 
+def pkgName():
+    ''' From this eg., filepath=.../bisos-pip/PkgName/py3/setup.py, extract PkgName. '''
+    filename = inspect.getframeinfo(inspect.currentframe()).filename
+    grandMother = pathlib.Path(filename).resolve().parent.parent.name
+    return f'bisos.{grandMother}'
 
-def readme():
-    with open('./README.org') as file:
-        while line := file.readline():
-            if match := re.search(r'^#\+title: (.*)',  line.rstrip()):
-                return match.group(1)
-            return "MISSING TITLE in ./README.org"
+def description():
+    ''' Extract title from ./_description.org which is expected to have a #+title: line. '''
+    try:
+        with open('./_description.org') as file:
+            while line := file.readline():
+                if match := re.search(r'^#\+title: (.*)',  line.rstrip()):
+                    return match.group(1)
+            return 'MISSING TITLE in ./_description.org'
+    except IOError:
+        return  'ERROR: Could not read ./_description.org file.'
 
 def longDescription():
+    ''' Read README.rst as a string. '''
+    fileName = './README.rst'
     try:
-        import pypandoc
-    except ImportError:
-        result = "warning: pypandoc module not found, could not convert to RST"
+        with open(fileName) as file:
+           result = file.read()
         return result
-    return pypandoc.convert_file('README.org', 'rst')
+    except IOError:
+        return  f'ERROR: Could not read {fileName} file.'
 
-####+BEGIN: b:py3:pypi/nextVersion :increment 0.01
-
-def pkgVersion(): return '0.11'
 ####+END:
 
-####+BEGIN: b:py3:pypi/requires :extras ()
+# b:py3:pypi:setup/version Arguments  :forSys t :forPyPi t :constant "666"
+####+BEGIN: b:py3:pypi:setup/version :comment "Auto Detected"
+
+# ./pypiUploadVer DID NOT exist -- forPypiVersion=0.11 -- forSysVersion=0.1 -- constant=NA
+def pkgVersion():
+        return '0.1'
+
+####+END:
+
+# b:py3:pypi:setup/requires :extras ; :requirements "requirements.txt" (bring here requirements.txt)
+####+BEGIN: b:py3:pypi:setup/requires :extras ()
 
 requires = [
 "blee",
@@ -35,24 +61,33 @@ requires = [
 "bisos.cntnr",
 "bisos.icm",
 "bisos.platform",
-"from",
 ]
 ####+END:
 
-####+BEGIN: b:py3:pypi/scripts :comment ""
+# b:py3:pypi:setup/scripts :comment
+####+BEGIN: b:py3:pypi:setup/scripts :comment ""
 
 scripts = [
 ]
 ####+END:
 
+# b:py3:pypi:setup/dataFiles :comment
+####+BEGIN: b:py3:pypi:setup/dataFiles :comment "Instead of ./MANIFEST.in or in pyproject.toml"
+
+data_files = [
+(' ',  ['lh-agpl3-LICENSE.txt', '_description.org', 'README.rst']),
+]
+####+END:
+
+# :pkgName "--auto--"  --- results in use of name=pkgName(),
+####+BEGIN: b:py3:pypi:setup/funcArgs :comment "defaults to --auto--"
 
 setuptools.setup(
-    name='bisos.pals',
+    name=pkgName(),  # bisos.pals
     version=pkgVersion(),
-    # namespace_packages=['bisos'],
     packages=setuptools.find_packages(),
     scripts=scripts,
-    requires=requires,
+    data_files=data_files,
     include_package_data=True,
     zip_safe=False,
     author='Mohsen Banan',
@@ -61,7 +96,7 @@ setuptools.setup(
     maintainer_email='libre@mohsen.1.banan.byname.net',
     url='http://www.by-star.net/PLPC/180047',
     license='AGPL',
-    description=readme(),
+    description=description(),
     long_description=longDescription(),
     download_url='http://www.by-star.net/PLPC/180047',
     install_requires=requires,
@@ -75,3 +110,5 @@ setuptools.setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         ]
     )
+
+####+END:
